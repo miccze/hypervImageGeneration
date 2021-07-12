c78973b0c8f5fc4129d154c134a4507a5ef2f582
Michał Czech <czechm11@gmail.com>
2021-07-12 22:43:19 +0200

    Tabs change

--- NewVM.ps1
+++ NewVM.ps1
@@ -20,57 +20,55 @@ $vSwitchName = "ExtSwitch"
 $VMpath = $TARGETDIR+"\"+$NanoName+"\VM" 
 
 
-Function Test-IsNetworkLocation
-        {
-            <#
-                .SYNOPSIS
-                    Determines whether or not a given path is a network location or a local drive.
-                .DESCRIPTION
-                    Function to determine whether or not a specified path is a local path, a UNC path,
-                    or a mapped network drive.
-                .PARAMETER Path
-                    The path that we need to figure stuff out about,
-            #>
+Function Test-IsNetworkLocation {
+	<#
+		.SYNOPSIS
+			Determines whether or not a given path is a network location or a local drive.
+		.DESCRIPTION
+			Function to determine whether or not a specified path is a local path, a UNC path,
+			or a mapped network drive.
+		.PARAMETER Path
+			The path that we need to figure stuff out about,
+	#>
 
-            [CmdletBinding()]
-            param(
-                [Parameter(ValueFromPipeLine = $true)]
-                [string]
-                [ValidateNotNullOrEmpty()]
-                $Path
-            )
+	[CmdletBinding()]
+	param(
+		[Parameter(ValueFromPipeLine = $true)]
+		[string]
+		[ValidateNotNullOrEmpty()]
+		$Path
+	)
 
-            $result = $false
+	$result = $false
 
-            if ([bool]([URI]$Path).IsUNC)
-            {
-                $result = $true
-            }
-            else
-            {
-                $driveInfo = [IO.DriveInfo]((Resolve-Path $Path).Path)
+	if ([bool]([URI]$Path).IsUNC)
+	{
+		$result = $true
+	}
+	else
+	{
+		$driveInfo = [IO.DriveInfo]((Resolve-Path $Path).Path)
 
-                if ($driveInfo.DriveType -eq "Network")
-                {
-                    $result = $true
-                }
-            }
+		if ($driveInfo.DriveType -eq "Network")
+		{
+			$result = $true
+		}
+	}
 
-            return $result
-        }
+	return $result
+}
+
 #Sprawdzanie czy ścieżka do VHDX jest lokalna czy sieciowa. W przypadku sieciowej, konieczne jest przekopiowanie obrazu loklanie. 
-if (Test-IsNetworkLocation $VHDXpath) 
-{
-
-$VHDX = Split-path $VHDXpath -Leaf
-$CopyDir = Join-Path -path $VMpath -ChildPath "VHDX"
-New-item $CopyDir -ItemType Directory -ErrorAction SilentlyContinue
-Copy-Item -Path $VHDXpath -Destination $CopyDir -Force
-$VHDXLocalPath = Join-Path -Path $CopyDir -ChildPath $VHDX
-#Tworzenie wirtualnej maszyny z obrazu przekopiowanego lokalnie. 
-New-VM -Name $NanoName -MemoryStartupBytes 1GB -BootDevice VHD -VHDPath $VHDXLocalPath -Path $VMpath -Generation 2 -SwitchName $vSwitchName
+if (Test-IsNetworkLocation $VHDXpath) {
+	$VHDX = Split-path $VHDXpath -Leaf
+	$CopyDir = Join-Path -path $VMpath -ChildPath "VHDX"
+	New-item $CopyDir -ItemType Directory -ErrorAction SilentlyContinue
+	Copy-Item -Path $VHDXpath -Destination $CopyDir -Force
+	$VHDXLocalPath = Join-Path -Path $CopyDir -ChildPath $VHDX
+	#Tworzenie wirtualnej maszyny z obrazu przekopiowanego lokalnie. 
+	New-VM -Name $NanoName -MemoryStartupBytes 1GB -BootDevice VHD -VHDPath $VHDXLocalPath -Path $VMpath -Generation 2 -SwitchName $vSwitchName
 } else {
-New-VM -Name $NanoName -MemoryStartupBytes 1GB -BootDevice VHD -VHDPath $VHDXpath -Path $VMpath -Generation 2 -SwitchName $vSwitchName
+	New-VM -Name $NanoName -MemoryStartupBytes 1GB -BootDevice VHD -VHDPath $VHDXpath -Path $VMpath -Generation 2 -SwitchName $vSwitchName
 }
 
 
